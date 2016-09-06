574ca743a2166eda09b7077daed32497fc6ffe10
lkndm <spiralta@gmail.com>
2016-09-06 20:51:41 +0300

    Внес небольшие правки

--- js/yandex-map.js
+++ js/yandex-map.js
@@ -2,9 +2,7 @@ ymaps.ready(init);
 
 function init() {
 
-  var myMap;
-
-  myMap = new ymaps.Map("map", {
+  var myMap = new ymaps.Map("map", {
     center: [59.938937, 30.319541],
     zoom: 17,
     controls: []
@@ -16,11 +14,12 @@ function init() {
     position: {top: 15, left: 15}
   });
 
-  var myPlacemark = new ymaps.Placemark([59.938632, 30.323055], {},
+  var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {},
         { iconLayout: "default#image",
           iconImageHref: "../img/map-marker.png",
           iconImageSize: [231, 190],
-          iconImageOffset: [-2, -4] });
+          iconImageOffset: [-2, -4]
+        });
 
   myMap.geoObjects.add(myPlacemark);
 }
